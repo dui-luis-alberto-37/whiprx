1c949b25cb9dfdd566e4645c0f8996f5554c5a2d
lgarciaorozco6 <lgarciaorozco6@gmail.com>
2025-05-19 20:16:25 -0600

    change app name & app views importations

---
+++ twitter/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponseRedirect
+from books.models import TwitterAccount
+
+# Create your views here.
